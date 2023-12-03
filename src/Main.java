9b4ff409cdb7f0cf88ded815ff98b37cc9a9821f
Danil Semenchin <semen4in.d@gmail.com>
2023-12-03 11:18:26 +0300

    increment plus synchronized thread

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        Task task = new Task();
+        Thread thread1 = new Thread(() -> task.firstThread());
 
+        Thread thread2 = new Thread(() -> task.secondThread());
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        task.showCounter();
     }
 }
